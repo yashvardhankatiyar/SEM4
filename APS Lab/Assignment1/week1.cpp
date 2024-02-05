c55755bd5f280b83b2525a941c6f88361fdef80b
yashvardhankatiyar <ykatiyar0902@gmail.com>
2024-02-05 17:52:15 +0530

    Added APS lab file for the first time

---
+++ APS Lab/Assignment1/week1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Sort the entire array using selection sort, bubble sort, insertion sort, quick sort and merge
+// sort. Print the total number of comparisons done in each of the sorting algorithm.
+
+// selection sort
+
+vector<int> selectionSort(vector<int> &v)
+{
+    
+}
