2e7d0fe9011b0a9d9c21753abfcdf6e71583599e
yashvardhankatiyar <ykatiyar0902@gmail.com>
2024-02-05 21:28:48 +0530

    commit 4 not a project

--- APS Lab/Assignment1/week1.cpp
+++ APS Lab/Assignment1/week1.cpp
@@ -8,5 +8,27 @@ using namespace std;
 
 vector<int> selectionSort(vector<int> &v)
 {
+    int min;
 
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        min = v[0];
+        for (int j = i + 1; j++; j < v.size())
+        {
+            if (v[j] < min)
+                swap(v[j], min);
+        }
+    }
+
+    return v;
+}
+
+int main()
+{
+    vector<int> v = {3, 5, 7, 3, 2, 1, 9, 8};
+    
+    //selection sort
+    v = selectionSort(v);
+    for(auto i: v)
+        cout<<i<<" ";
 }
